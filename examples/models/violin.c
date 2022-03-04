1de7ecf1dffb2116e9ddfd53d805f6e61422e79b
MCestradad <MCestradad@email.com>
2022-03-04 14:36:35 -0500

    V6

---
+++ examples/models/violin.c
@@ -0,0 +1,49 @@
+/**
+ * Whenever a director MOVES, violins look at him. For each violin present,
+ * register for all of the directors present.
+ */
+
+#include "violin.h"
+
+static void _destroy(Violin* this)
+{
+	this->observer->destroy(this->observer);
+
+	if (this != NULL){
+		free(this);
+		this = NULL;
+	}
+}
+
+static void _smell(Violin * this, Director* director)
+{
+	director->registerObserver(director, this->observer);
+	printf("%s Just saw %s\n", this->name, director->name);
+}
+
+static void _handleDirectorEvent(Violin* this, Director* cat)
+{
+	printf("%s\n just heared %s", this->name, director->name);
+}
+
+
+/**
+ * Observer Method called upon the receiption of an incoming event.
+ * @param type
+ * @param subject
+ */
+static void _notify(Violin* this, int numero, void* subject) {
+	_handleCatEvent(this, (Director*) subject);
+}
+
+Violin* VIOLIN_create(char* violinsName)
+{
+	Violin* this = (Violin*) malloc(sizeof(*this));
+
+	this->name = violinsName;
+	this->see = _see;
+	this->destroy = _destroy;
+	this->observer = observerNew(this, (void (*)(void*, int, void*))_notify);
+
+	return this;
+}
